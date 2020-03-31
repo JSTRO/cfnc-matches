1f6b66d61b42ff03c9ada0f0ebe00b832b5bfbe8
Joseph Ostrow <jstro@umich.edu>
2020-03-31 17:27:53 -0400

    Updates

---
+++ precache-manifest.3cd56a9402ef0ddb7901eef4923a4d34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d3a6b916a48bd9073d78c9cd699f21",
+    "url": "/cfnc-matches/index.html"
+  },
+  {
+    "revision": "26a252db69663b0ce899",
+    "url": "/cfnc-matches/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6f7b4ffe9ad9a19c1895",
+    "url": "/cfnc-matches/static/css/main.ae5e79fc.chunk.css"
+  },
+  {
+    "revision": "26a252db69663b0ce899",
+    "url": "/cfnc-matches/static/js/2.c55b2f75.chunk.js"
+  },
+  {
+    "revision": "d140161135057d608e30f06179a31c50",
+    "url": "/cfnc-matches/static/js/2.c55b2f75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f7b4ffe9ad9a19c1895",
+    "url": "/cfnc-matches/static/js/main.88df100b.chunk.js"
+  },
+  {
+    "revision": "3b7ad81e0c85ee3e2fdb",
+    "url": "/cfnc-matches/static/js/runtime-main.1e68b5b8.js"
+  }
+]);
