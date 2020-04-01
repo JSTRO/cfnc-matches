baae9890abbf526d295355349977a49cf0c37310
Joseph Ostrow <jstro@umich.edu>
2020-04-01 01:47:26 -0400

    swap in new csv export function and change output format

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button'
 import papa from 'papaparse'
+import converter from 'convert-array-to-csv'
 import { uniq, uniqBy } from 'lodash'
+import exportToCsv from './exportToCsv.js'
 
 function App() {
 
@@ -24,56 +26,73 @@ function App() {
     if (matches.length < 1) {
       alert("Please choose a file.")
     } else {
-      let csvContent = "data:text/csv;charset=utf-8," + matches.map(e => e.join(",")).join("\n")
-      const encodedUri = encodeURI(csvContent)
-      window.open(encodedUri)
+      exportToCsv('download.csv', matches)
     }
   }
 
-  const findMatches = rows => {
-    const data = rows.map(row1 => {
-      const row1_email = row1['Email Address']
-      const row1_gender = row1['I am a']
-      const row1_age = row1['I am aged']
-      const row1_pref = row1['I am interested in dating a']
-      const row1_interest = row1['I am interested in dating someone aged ']
-      const row1_religion_important = row1['It is important that my partner is the same religion as me']
-      const row1_religion = row1['What religion are you?']
-      const row1_children = row1['Do you want to have children?']
-      const row1_politics_important = row1['It is important that my partner has the same political beliefs as I do']
-      const row1_politics = row1['If so, my political beliefs are']
-      return ([
-        row1_email,
-        rows.filter(row2 => { // TODO: trim and standardize case
-          const row2_email = row2['Email Address']
-          const row2_gender = row2['I am a']
-          const row2_age = row2['I am aged']
-          const row2_pref = row2['I am interested in dating a']
-          const row2_interest = row2['I am interested in dating someone aged ']
-          const row2_religion_important = row2['It is important that my partner is the same religion as me']
-          const row2_religion = row2['What religion are you?']
-          const row2_children = row2['Do you want to have children?']
-          const row2_politics_important = row2['It is important that my partner has the same political beliefs as I do']
-          const row2_politics = row2['If so, my political beliefs are']
-          return (
-            (row1_email !== row2_email) && // Cannot match with yourself
-            (row1_pref === row2_gender) && (row1_gender === row2_pref) &&
-            (row1_interest === row2_age) && (row1_age === row2_interest) &&
-            ((row1_religion_important === 'Yes' || row2_religion_important === 'Yes') ? row1_religion === row2_religion : null) &&
-            ((row1_politics_important === 'Yes' || row2_politics_important === 'Yes') ? row1_politics === row2_politics : null) &&
-            (row1_children === row2_children) 
-          )
-        }).map(row => row['Email Address'])
-      ])
-    })
+  // Convert form column headers into variables
+  const email = 'Email Address'
+  const gender = 'I am a'
+  const age = 'I am aged'
+  const gender_pref = 'I am interested in dating a'
+  const age_pref = 'I am interested in dating someone aged '
+  const religion_important = 'It is important that my partner is the same religion as me'
+  const religion = 'What religion are you?'
+  const children = 'Do you want to have children?'
+  const politics_important = 'It is important that my partner has the same political beliefs as I do'
+  const politics = 'If so, my political beliefs are'
 
-    let matches = data.filter(match => match[1].length > 0) // Remove rows with no matches
-                      .map(match => [match[0], uniq(match[1])]) 
-    matches = uniqBy(matches, item => JSON.stringify(item)) // Only include unique entries
+  const findMatches = rows => {
+    const matches = rows.map(row1 => {
+      return ([
+        row1[email],
+        rows.filter(row2 => {
+          return (
+            (row1[email] !== row2[email]) && // Cannot match with yourself
+            (row1[gender_pref] === row2[gender]) && 
+            (row1[gender] === row2[gender_pref]) &&
+            (row1[age_pref] === row2[age]) && 
+            (row1[age] === row2[age_pref]) &&
+            ((row1[religion_important] === 'Yes' || row2[religion_important] === 'Yes') ? row1[religion] === row2[religion] : null) &&
+            ((row1[politics_important] === 'Yes' || row2[politics_important] === 'Yes') ? row1[politics] === row2[politics] : null) &&
+            (row1[children] === row2[children]) 
+          )
+        }).map(row => row[email])
+      ])
+    }).filter(match => match[1].length > 0) // Remove rows with no matches
+    
+    // Format as all possible matches                 
+    let output = []
+    for (let i=0; i<matches.length; i++) {
+      const a = matches[i][0]
+      const b = matches[i][1]
+      if (b.length > 1) {
+        for (let j=0; j<b.length; j++) {
+          output.push([a, b[j]])
+        }
+      } else {
+        output.push([a, b[0]])
+      }
+    }
+
+    // Only include unique entries
+    output = uniqBy(output, item => JSON.stringify(item))
+    
+    // Only include one side of matches
+    for (let i=0; i<output.length; i++) {
+      for (let j=0; j<output.length; j++) {
+        if (output[i][0] === output[j][1] && output[i][1] === output[j][0]) {
+          output.splice(output.indexOf(output[j]), 1)
+        }
+      }
+    }
+
     // Add headers
-    matches.unshift(['email', 'matches'])
-    return matches
+    output.unshift(['1', '2'])
+
+    return output
   } 
+
   return (
     <div className="App">
       <div className="container-fluid">
